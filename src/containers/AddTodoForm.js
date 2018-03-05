6a0a0717add0e41c63425de70d0cd22583854840
Viktor Zhurbin <vzhurbin@gmail.com>
2018-03-05 08:40:24 +0300

    add form class to function & lint code

--- src/containers/AddTodoForm.js
+++ src/containers/AddTodoForm.js
@@ -2,21 +2,21 @@ import React from 'react';
 import SelectBox from '../components/SelectBox';
 import TextBox from '../components/TextBox';
 
-export default class AddTodoForm extends React.Component {
-  render() {
-    const text = this.props.text;
-    const label = this.props.label;
-    return (
-      <form onSubmit={e => this.props.addTodo(e)}>
-        <SelectBox label={label} onChange={e => this.props.onLabelChange(e)} />
-        <TextBox
-          value={text}
-          placeholder="What to do?"
-          onChange={e => this.props.onInputChange(e)}
-          autoFocus
-        />
-        <button>+ Add</button>
-      </form>
-    );
-  }
-}
+const AddTodoForm = props => {
+  const text = props.text;
+  const label = props.label;
+  return (
+    <form onSubmit={e => props.addTodo(e)}>
+      <SelectBox label={label} onChange={e => props.onLabelChange(e)} />
+      <TextBox
+        value={text}
+        placeholder="What to do?"
+        onChange={e => props.onInputChange(e)}
+        autoFocus
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default AddTodoForm;
